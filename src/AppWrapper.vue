74a39a3ecc2286a6fa700907eff7a6839f5f5826
Alexey Surkov <alexprakticum@yandex.ru>
2026-03-03 22:03:53 +0300

    fix

--- src/AppWrapper.vue
+++ src/AppWrapper.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
 import { useAuthStore } from '@/stores/auth'
 import { useMediaStore } from '@/stores/media'
 
 const authStore = useAuthStore()
 const mediaStore = useMediaStore()
-
-// onMounted(async () => {
-//   await authStore.initialize()
-//   //await mediaStore.fetchUserMedia()
-// })
-
-// if (authStore.user) {
-//   await mediaStore.fetchUserMedia()
-// }
 
 await authStore.initialize()
 await new Promise(resolve => setTimeout(resolve, 2000))
